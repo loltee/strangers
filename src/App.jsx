02d81991e4d65b612e268f8b29f5df4e875c1851
tracy kendrick <tracykendrick0@gmail.com>
2023-08-19 14:13:33 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,13 @@ import PostCreate from './components/PostCreate';
 import Post from './components/Post'
 import {getallposts} from './api/requests';
 import Signup from './components/SignUp';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import MessageForm from './components/MessageForm';
 
 
 
 
 
-// const MessageForm = ({ postId, onAddMessage }) => {
-//   const [message, setMessage] = useState('');
-
-//   const handleMessageChange = (event) => {
-//     setMessage(event.target.value);
-//   };
-
-//   const handleAddClick = () => {
-//     onAddMessage(postId, message);
-//     setMessage('');
-//   };
-
-//   return (
-//     <div>
-//       <h3>Add Message</h3>
-//       <textarea
-//         value={message}
-//         onChange={handleMessageChange}
-//         placeholder="Enter your message..."
-//       />
-//       <button onClick={handleAddClick}>Add</button>
-//     </div>
-//   );
-// };
 
 const Write = ({ post, onDelete, onUpdate, onAddMessage }) => {
   console.log("in right component",post)
@@ -70,39 +46,19 @@ const [token, setToken] = useState(window.localStorage.getItem("token")|| null);
         window.localStorage.removeItem("token")
       }
     }, [token])
-
-    // fetch(`${BASE_URL}/posts`)
-    //   .then(response => response.json())
-    //   .then(data => setPosts(data))
-    //   .catch(error => console.error('Error fetching data:', error));
-    // }, [deletedPostId]);
-
-  // const handleDelete = (postId) => {
-  //   fetch(`${BASE_URL}/posts/${postId}`, {
-  //     method: 'DELETE'
-  //   })
-  //     .then(response => {
-  //       if (response.status === 204) {
-  //         setDeletedPostId(postId);
-  //       } else {
-  //         console.log('Error deleting post:', response.status);
-  //       }
-  //     })
-  //     .catch(error => console.error('Error deleting post:', error));
-  // };
-
- 
-
- 
- 
+    
+    
  
   console.log(token)
   return (
     
     <div>
 
-<h1>Stranger Things</h1>
-
+<h1>Strangers Things</h1>
+<nav>
+    <Link to='/' element={<PostList/>}>All posts </Link>
+    <Link to="/sign" element={<Signup/>}>Sign up</Link>
+  </nav>
     <Routes>
       <Route path='/' element={<PostList/>}/>
       <Route path='/sign' element={<Signup setToken={setToken}/>}/>
